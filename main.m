025fc798faf83377194864ab609861e678a0ad1c
Robert Boone <robo4288@gmail.com>
2009-09-29 07:22:41 -0500

    Initial commit

---
+++ main.m
@@ -0,0 +1,14 @@
+//
+//  main.m
+//  delUser
+//
+//  Created by Robert Boone on 9/29/09.
+//  Copyright __MyCompanyName__ 2009. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+int main(int argc, char *argv[])
+{
+    return NSApplicationMain(argc,  (const char **) argv);
+}
